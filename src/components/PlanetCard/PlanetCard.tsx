089119e56c28c492b49f24c52dc8346c3dd4fe70
Frederic Briand <fbr91144@gmail.com>
2026-06-19 12:05:11 +0200

    moons svg path

--- src/components/PlanetCard/PlanetCard.tsx
+++ src/components/PlanetCard/PlanetCard.tsx
@@ -13,43 +13,49 @@ function PlanetCard ({ planet, moon } : { planet: Planet, moon: Moon}) {
             </div>
             <div className={styles['block']}>
                 <div>
-                    <span className={styles['label']}>{Labels.diameter}</span>
-                    <span className={styles['field']}>{planet.diameter}</span>
+                    <span className={styles['label']}>{ Labels.diameter }</span>
+                    <span className={styles['field']}>{ planet.diameter }</span>
                 </div>
                 <div>
-                    <span className={styles['label']}>{Labels.mass}</span>
-                    <span className={styles['field']}>{planet.mass}</span>
+                    <span className={styles['label']}>{ Labels.mass }</span>
+                    <span className={styles['field']}>{ planet.mass }</span>
                 </div>
             </div>
             <div className={styles['block']}>
                 <div>
-                    <span className={styles['label']}>{Labels.minTemperature}</span>
-                    <span className={styles['field']}>{planet.minTemperature}</span>
+                    <span className={styles['label']}>{ Labels.minTemperature }</span>
+                    <span className={styles['field']}>{ planet.minTemperature }</span>
                 </div>
                 <div>
-                    <span className={styles['label']}>{Labels.maxTemperature}</span>
-                    <span className={styles['field']}>{planet.maxTemperature}</span>
+                    <span className={styles['label']}>{ Labels.maxTemperature }</span>
+                    <span className={styles['field']}>{ planet.maxTemperature }</span>
                 </div>
             </div>
             <div className={styles['block']}>
                 <div>
-                    <span className={styles['label']}>{Labels.rotationDuration}</span>
-                    <span className={styles['field']}>{planet.rotationDuration}</span>
+                    <span className={styles['label']}>{ Labels.rotationDuration }</span>
+                    <span className={styles['field']}>{ planet.rotationDuration }</span>
                 </div>
                 <div>
-                    <span className={styles['label']}>{Labels.revolutionDuration}</span>
-                    <span className={styles['field']}>{planet.revolutionDuration}</span>
+                    <span className={styles['label']}>{ Labels.revolutionDuration }</span>
+                    <span className={styles['field']}>{ planet.revolutionDuration }</span>
                 </div>
             </div>
-            {planet.moons && planet.moons.length > 0 && (
+            { planet.moons && planet.moons.length > 0 && (
                 <div className={styles['bottom']}>
                     <div>
-                        <span className={styles['label']}>{Labels.moonsCount}</span>
-                        <span className={styles['field']}>{planet.moonsCount}</span>
+                        <span className={styles['label']}>{ Labels.moonsCount }</span>
+                        <span className={styles['field']}>{ " " + planet.moonsCount }</span>
                     </div>
-                    {planet.moons.map(moon => (
-                        <li key={moon.id} className={styles['field']}>{moon.name}</li>
-                    ))}
+                    <ul>
+                        { planet.moons.map(moon => (
+                            <li key={ moon.id } className={styles['field']}>
+                                <img src={ moon.imageUrl } alt={ moon.name } />
+                                <span>{ moon.name }</span>
+                                <span>{ moon.diameter } km</span>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             )}
         </article>
