d91b32ac49133f6965dcc81451742e7bb473a484
Frederic Briand <fbr91144@gmail.com>
2026-06-19 11:38:33 +0200

    moon display

--- src/components/PlanetCard/PlanetCard.tsx
+++ src/components/PlanetCard/PlanetCard.tsx
@@ -1,7 +1,9 @@
 import styles from "./PlanetCard.module.scss";
-import type { Planet } from "../../types/planet";
+import type { Planet, Moon } from "../../types/planet";
 
-function PlanetCard ({ planet } : { planet: Planet}) {
+import Labels from '../../data/labels.json';
+
+function PlanetCard ({ planet, moon } : { planet: Planet, moon: Moon}) {
     return (
         <article className={`planet-card-${planet.name.toLowerCase()} ${styles['article']}`}>
             <div className={styles['visual']}>
@@ -9,34 +11,49 @@ function PlanetCard ({ planet } : { planet: Planet}) {
                 <h3>{ planet.name }</h3>
                 <span>{ planet.type }</span>
             </div>
-            <div>
-                <span>{ planet.diameter }</span>
-                <span>{ planet.mass }</span>
+            <div className={styles['block']}>
+                <div>
+                    <span className={styles['label']}>{Labels.diameter}</span>
+                    <span className={styles['field']}>{planet.diameter}</span>
+                </div>
+                <div>
+                    <span className={styles['label']}>{Labels.mass}</span>
+                    <span className={styles['field']}>{planet.mass}</span>
+                </div>
             </div>
-            <div>
-                <span>{ planet.minTemperature }</span>
-                <span>{ planet.maxTemperature }</span>
+            <div className={styles['block']}>
+                <div>
+                    <span className={styles['label']}>{Labels.minTemperature}</span>
+                    <span className={styles['field']}>{planet.minTemperature}</span>
+                </div>
+                <div>
+                    <span className={styles['label']}>{Labels.maxTemperature}</span>
+                    <span className={styles['field']}>{planet.maxTemperature}</span>
+                </div>
             </div>
-            <div>
-                <span>{ planet.rotationDuration }</span>
-                <span>{ planet.revolutionDuration }</span>                
+            <div className={styles['block']}>
+                <div>
+                    <span className={styles['label']}>{Labels.rotationDuration}</span>
+                    <span className={styles['field']}>{planet.rotationDuration}</span>
+                </div>
+                <div>
+                    <span className={styles['label']}>{Labels.revolutionDuration}</span>
+                    <span className={styles['field']}>{planet.revolutionDuration}</span>
+                </div>
             </div>
+            {planet.moons && planet.moons.length > 0 && (
+                <div className={styles['bottom']}>
+                    <div>
+                        <span className={styles['label']}>{Labels.moonsCount}</span>
+                        <span className={styles['field']}>{planet.moonsCount}</span>
+                    </div>
+                    {planet.moons.map(moon => (
+                        <li key={moon.id} className={styles['field']}>{moon.name}</li>
+                    ))}
+                </div>
+            )}
         </article>
     )
 }
 
 export default PlanetCard;
-
-// "name": "Mercure",
-//     "type": "tellurique",
-//     "diameter": 4879,
-//     "mass": "3.30 x 10^23 kg",
-//     "distanceFromSun": "57.9 millions km",
-//     "minTemperature": -180,
-//     "maxTemperature": 430,
-//     "rotationDuration": "58.6 jours",
-//     "revolutionDuration": "88 jours",
-//     "moonsCount": 0,
-//     "rings": false,
-//     "imageUrl": "https://cdn-icons-png.flaticon.com/128/2949/2949047.png",
-//     "moons": []
